246ad6a46f19e9ce93f8666ae6c9ad6bdd997cee
LegendaMichal <dareos1994@gmail.com>
2020-04-25 23:12:56 +0200

    uprava web socket portu

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ const morgan = require('morgan');
 const path = require('path');
 
 const app = express();
+const server = createServer(app);
+var io = require('socket.io')(server);
 
 // Express server
 const normalizePort = port => parseInt(port, 10);
-const PORT = normalizePort(process.env.PORT || 5000);
+const PORT = normalizePort(process.env.PORT || 80);
 
 app.disable('x-powered-by');
 app.use(compression());
@@ -20,24 +22,15 @@ app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
 
-const server = createServer(app);
 
-server.listen(PORT, err => {
-    if (err) throw err;
-
-    console.log("Server started on port", PORT);
-});
 
 // Web Socket server
-const port = 3011;
-var servers = require('http').createServer(app);
-var io = require('socket.io')(servers);
-const UserList = require('./src/server/userlist.js');
-var users = new UserList();
+// const UserList = require('./src/server/userlist.js');
+// var users = new UserList();
 
 io.on('connection', socket => {
-    users.addUser(socket.id);
-    console.log(users);
+    // users.addUser(socket.id);
+    // console.log(users);
     socket.join("abc");
     socket.emit('my_id', { id: socket.id });
     socket.broadcast.to('abc').emit('player_join', { id: socket.id });
@@ -45,7 +38,7 @@ io.on('connection', socket => {
     socket.on('disconnect', reason => {
         socket.leaveAll();
         socket.broadcast.to('abc').emit('player_left', { id: socket.id });
-        users.removeUser(socket.id);
+        // users.removeUser(socket.id);
     });
 
     socket.on('player_data', data => {
@@ -53,4 +46,9 @@ io.on('connection', socket => {
     })
 });
 
-servers.listen(port, () => { console.log("Socket server start on port:", port)});
+
+server.listen(PORT, err => {
+    if (err) throw err;
+
+    console.log("Server started on port", PORT);
+});
--- src/components/jetsfight.js
+++ src/components/jetsfight.js
@@ -8,7 +8,7 @@ const KEY = {
 };
 
 const io = require('socket.io-client');
-const socket = io('http://localhost:3011');
+const socket = io();
 
 export default class JetsFightGame extends Component {
   constructor() {
